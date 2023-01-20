7c7deb047f7127034456e0732ea83cfcf2065f46
TimoLaasonen <timo.laasonen@elenium.fi>
2023-01-20 16:36:29 +0200

    security config without csrf

---
+++ src/main/java/fi/example/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package fi.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+
+import java.time.Duration;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+
+        http
+            .csrf().disable()
+            .cors()
+            .and()
+            .authorizeHttpRequests()
+            .anyRequest()
+            .permitAll()
+            .and()
+            .headers(headers -> headers
+                .httpStrictTransportSecurity(hsts -> hsts
+                    .includeSubDomains(true)
+                    .maxAgeInSeconds(Duration.ofDays(365).getSeconds())
+                    .requestMatcher(AnyRequestMatcher.INSTANCE)
+                )
+                .referrerPolicy(referrer -> referrer
+                    .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER)
+                )
+            );
+
+        return http.build();
+    }
+}
+
